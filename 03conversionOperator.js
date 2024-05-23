25aa1edbd95c0d65ec5c70df5687ffd0e0e89c05
Sagar Sharma <118193567+29SagarSharma@users.noreply.github.com>
2024-05-23 10:46:08 +0530

    Add files via upload

---
+++ 03conversionOperator.js
@@ -0,0 +1,27 @@
+let score = undefined //"33","33abc", "Null", "undefined", "true"
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+
+let valueInNumber = Number(score) //Conversion in to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) 
+
+//"33" =>33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=> true ; 0 => false 
+// "" => false
+// "hitesh" => true
+ let someNumber = 33
+ let stringNumber = String(someNumber) //converted to string
+ console.log(stringNumber);
+ console.log(typeof stringNumber); // type pta krne ke liye
+
+ 
