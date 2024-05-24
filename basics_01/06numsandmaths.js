e006bdeaffa11be7a6356b2a8933f776042d98c9
Sagar Sharma <118193567+29SagarSharma@users.noreply.github.com>
2024-05-24 17:29:34 +0530

    Add files via upload

---
+++ basics_01/06numsandmaths.js
@@ -0,0 +1,38 @@
+const score= 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(3)) //23.9
+
+const otherNumber1 = 123.8966
+//console.log(otherNumber1.toPrecision(3))// 124
+
+const otherNumber2 = 1123.8966
+//console.log(otherNumber2.toPrecision(3)) // 1.12e+3
+
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN')); //1,00,000
+
+// **********************Maths**********************
+
+// console.log(Math) //Object [Math] {}
+
+
+// console.log(Math.abs(-4)) //absolute vale neg to pos
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2))//5
+// console.log(Math.floor(4.9))//4
+// console.log(Math.min(4,3,6,8))//3
+// console.log(Math.max(4,3,6,8))//8
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
